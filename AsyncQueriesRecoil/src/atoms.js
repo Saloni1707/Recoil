44fd1d676f318a469a3d17b29c683bd893bd29ef
Saloni Atole <145243379+Saloni1707@users.noreply.github.com>
2024-11-09 20:10:59 +0530

    Update atoms.js

--- AsyncQueriesRecoil/src/atoms.js
+++ AsyncQueriesRecoil/src/atoms.js
@@ -1,12 +1,23 @@
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
-    key: "networkAtom",
+    key: "networkAtom",    //here this is the basic code which shows some lag,naive way of doing things!
     default: {
         network: 4, 
         jobs: 6, 
         messaging: 3, 
         notifications: 3
+    }
+});
+
+export const notifications = atom ({
+    key:"networkAtom",    //this is the optimal approach with the Async function in the Selector :)
+    default: {
+        key : "networkAtomSelector",
+        get : async () => { //why do this ? kyunki we can't use directly the async func so used a Selector here
+            const res = await axios.get("https://sum-server.100xdevs.com/notifications")
+            return res.data
+        }
     }
 });
 
